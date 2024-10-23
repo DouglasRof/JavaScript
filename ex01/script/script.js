ac5614ac7e49feb3aca578c7987159ac194e4c27
Douglas Rodrigues <douglas.rodrigues.99@hotmail.com>
2024-10-23 20:24:07 -0300

    ex01 - aula 12

--- ex01/script/script.js
+++ ex01/script/script.js
@@ -1,13 +1,15 @@
-var hora = new Date()
-var agora = 14
-var infoh = document.getElementById('info')
-var img = document.getElementById('img')
-infoh.innerHTML = `<p> Agora são ${agora} Horas`
-if (agora > 6 && agora < 12){
-        img.innerHTML += '<style> div#img > #manha { display: block;} </style>'
-    }else if (agora > 12 && agora < 18) {
-        img.innerHTML += '<style> div#img > #manha { display: none;} </style> <style> div#img > #tarde { display: block;} </style>'}
-    else (agora > 18 && agora < 6) {
-        img.innerHTML += '<style> div#img > #manha { display: none;} </style> <style> div#img > #tarde { display: none;} </style> <style> div#img > #noite { display: block;} </style>'
+function carregar(){
+    var hora = new Date()
+    var agora = hora.getHours()
+    var infoh = document.getElementById('info')
+    var img = document.getElementById('img')
+    infoh.innerHTML = `<p> Agora são ${agora} Horas`
+        if (agora > 12 && agora <= 18){
+            img.innerHTML += '<style>body{ background-image: linear-gradient(#BA94B2,#F5A974);} div#img > img#manha {display: none;} div#img > img#noite {display: none;} </style><center><strong><p style="color:darkred; font-size: 1.5em;">Boa Tarde!</p></center>'
+            
+        }else if (agora > 18 || agora < 5){
+            img.innerHTML += '<style>body{ background-image: linear-gradient(#49548C,#5C76B1);}  div#img > img#manha {display: none;} div#img > img#tarde {display: none;} </style><center><strong><p style="color:#49548C; font-size: 1.5em;">Boa Noite!</p></center>'
+        }else{
+            img.innerHTML += '<style>body{ background-image: linear-gradient(dodgerblue, white);} div#img > img#noite {display: none;} div#img > img#tarde {display: none;} </style><center><strong><p style="color:dodgerblue; font-size: 1.5em;">Bom Dia!</p></center>'
+        } 
     }
-    
