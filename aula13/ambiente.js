381398eda883cc1cded9a0d1bcfe07a3a259728f
Douglas Rodrigues <douglas.rodrigues.99@hotmail.com>
2024-10-27 15:05:09 -0300

    att

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while(c <= 3)
+
+/* var c = 1
+while(c <= 3){
+    console.log(`Passo ${c}`)
+    c++
+} */
+
+/*
+console.log ('passo 1')
+console.log ('passo 2')
+console.log ('passo 3')
+*/
